dc2c7f17bdbd01b4ed8a990e70035757183ec190
Simdev <simonetoa04@gmail.com>
2025-04-09 13:06:07 +0100

    stagger v1 finish

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import img3 from './assets/3.svg'
 import img4 from './assets/4.svg'
 import img5 from './assets/5.svg'
 import img6 from './assets/6.svg'
+import { stagger } from 'motion'
 
 function App() {
   const [scope, animate] = useAnimate()
@@ -21,18 +22,24 @@ function App() {
   //     [".iml", { y: -396 }, { duration: 3, ease: "easeOut", at: "<" }]
   //   ])
   // }, [animate])
-  
-  const handlePlayAnim = ()=>{
+
+  const handlePlayAnim = () => {
     animate([
-      [".imp", { y: -400 }, { duration: 1, ease: "easeOut" }],
-      [".other", { y: -400 }, { duration: .8, ease: "easeOut", at:"-0.6" }],
-      [".other", { y: 400 }, { duration: 0.000001 }],
-      [".imp", { y: 0 }, { duration: 0.000001 }],
-      [".iml", { y: 0 }, { duration:0.000001 }]
+      [".imp", { y: -400, opacity: .8 }, { duration: 1, ease: "easeOut" }],
+      [".imp1", { y: -400, opacity: .8 }, { duration: 1, ease: "easeOut", at: "<" }],
+      [".other", { y: -800, opacity: .8 }, {duration:1, delay:stagger(0.1), at:"-1" }],
+      [".img2",{opacity:1}, {duration:.6, ease:"easeOut", at:"-1"}],
+      [".other", { y: 400, opacity: 1 }, { duration: 0.000001, at:0.5 }],
+      [".imp", { y: 0, opacity: 1 }, { duration: 0.000001 }],
+      [".iml", { y: 0, opacity: 1 }, { duration: 0.000001 }],
+      [".imp1", { y: 400, opacity: 1 }, { duration: 0.000001, }],
+      [".img2",{opacity:0}, {duration:.6, ease:"easeOut", at:"-1"}],
     ])
+    
+    // animate(".other", { y: -400, opacity: 0 }, {duration:.8, delay: stagger(0.1) })
   }
   return (
-    <div ref={scope} className='w-screen overflow-clip'>
+    <div ref={scope} className='w-screen overflow-hidden'>
       <header className='flex items-center px-12 py-4 justify-between'>
         <div className='flex text-xl'>
           <h3 className='font-bold mr-4'>Logo</h3>
@@ -61,42 +68,43 @@ function App() {
             <button onClick={handlePlayAnim} className='py-2 px-4 bg-transparent border-[1.5px] border-gray-400 rounded-full'>Watch our platform</button>
           </div>
         </div>
-        <div className='h-[400px] grid grid-cols-12 relative overflow-clip'>
-          <div className="w-full h-full bg-[url('./assets/girl1.svg')] bg-cover bg-center absolute"></div>
-          <div className='z-10'></div>
+        <div className='h-[400px] grid grid-cols-12 relative overflow-hidden'>
+          <motion.div className="w-full h-full bg-[url('./assets/girl1.svg')] bg-cover bg-center absolute"></motion.div>
+          <motion.div initial={{opacity:0}} className="img2 w-full h-full bg-[url('./assets/girl2.svg')] bg-cover bg-center absolute"></motion.div>
           <div className='z-10'>
-            <motion.div initial={{y:400}} className='opacity-80 other w-full h-[800px] bg-linear-to-t from-[#0c547c] to-[#ecc2c8]  '></motion.div>
+            <motion.div initial={{ y: 400 }} className='other w-full h-[800px] bg-linear-to-t from-[#0c547c] to-[#83d6e8] '></motion.div>
           </div>
           <div className='z-10'>
-
+            <motion.div initial={{ y: 400 }} className='other w-full h-[800px] bg-linear-to-t from-[#0c547c] to-[#ecc2c8]  '></motion.div>
           </div>
           <div className='z-10'>
-            <motion.div initial={{y:400}}  className='opacity-80 other w-full h-[800px] bg-linear-to-t from-[#0c547c] to-[#ecc2c8]  '></motion.div>
+            <motion.div initial={{ y: 400 }} className='other w-full h-[800px] bg-linear-to-t from-[#0c547c] to-[#83d6e8] '></motion.div>
           </div>
           <div className='z-10'>
-
+            <motion.div initial={{ y: 400 }} className='other w-full h-[800px] bg-linear-to-t from-[#0c547c] to-[#ecc2c8]  '></motion.div>
           </div>
           <div className='z-10'>
-            <motion.div initial={{y:400}} className='opacity-80 other w-full h-[800px] bg-linear-to-t from-[#0c547c] to-[#ecc2c8] '></motion.div>
+            <motion.div initial={{ y: 400 }} className='other w-full h-[800px] bg-linear-to-t from-[#0c547c] to-[#83d6e8] '></motion.div>
           </div>
           <div className='z-10'>
-
+            <motion.div initial={{ y: 400 }} className='other w-full h-[800px] bg-linear-to-t from-[#0c547c] to-[#ecc2c8] '></motion.div>
           </div>
           <div className='z-10'>
-          <motion.div initial={{y:400}} className='opacity-80 other w-full h-[800px] bg-linear-to-t from-[#0c547c] to-[#ecc2c8] '></motion.div>
-            
+            <motion.div initial={{ y: 400 }} className='other w-full h-[800px] bg-linear-to-t from-[#0c547c] to-[#83d6e8] '></motion.div>
           </div>
           <div className='z-10'>
-
+            <motion.div initial={{ y: 400 }} className='other w-full h-[800px] bg-linear-to-t from-[#0c547c] to-[#ecc2c8] '></motion.div>
           </div>
           <div className='z-10'>
-          <div className='imp w-full h-full bg-linear-to-t from-[#815d4f] via-80% via-[#815d4f] to-95% to-[#d8e6f5]'></div>
+            <motion.div initial={{ y: 400 }} className='other w-full h-[800px] bg-linear-to-t from-[#0c547c] to-[#83d6e8] '></motion.div>
           </div>
           <div className='z-10'>
-
+            <div className='imp w-full h-full bg-linear-to-t from-[#815d4f] via-80% via-[#815d4f] to-95% to-[#d8e6f5]'></div>
           </div>
           <div className='z-10'>
-            {/* #a2857b #83d6e8 #0c547c */}
+            <motion.div initial={{ y: 400 }} className='imp1 w-full h-[800px] bg-linear-to-t from-[#0c547c] to-[#83d6e8] '></motion.div>
+          </div>
+          <div className='z-10'>
             <div className='imp w-full h-full bg-linear-to-t from-[#83d6e8]  to-[#ecc2c8]'></div>
           </div>
         </div>
